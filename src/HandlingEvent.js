eb33cc408260edf2202275aa9e99cb537e4bd661
faisd405 <faisd405@gmail.com>
2021-07-09 09:41:08 +0700

    penambahan handling event

--- src/HandlingEvent.js
+++ src/HandlingEvent.js
@@ -1,13 +1,49 @@
+import React from 'react';
 import  './App.css';
 
+
 function HandlingEvent() {
+  return(
+    <Submit />,
+    <Toggle />
+    )
+} 
+
+function Submit(){
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+    alert("You clicked submit!");
+  }
+
   return (
-    
-    <div className="HandlingEvent">
-        <button onclick={activateLasers()}>
-            Activate Lasers
-        </button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
+
+class Toggle extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // cara binding seperti ini diperlukan untuk membuat `this` dapat berfungsi -
+    // pada callback binding
+    this.handleClick = this.handleClick.bind(this); 
+  }
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
 export default HandlingEvent;
